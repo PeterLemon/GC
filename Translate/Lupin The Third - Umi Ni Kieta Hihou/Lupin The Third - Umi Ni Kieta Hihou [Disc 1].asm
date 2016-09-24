d2a7cae31aacac4f81f077893815454e2a1eccf4
PeterLemon <krom@gbadev.org>
2016-09-24 19:59:44 +0100

    Lupin III Credits

--- Translate/Lupin The Third - Umi Ni Kieta Hihou/Lupin The Third - Umi Ni Kieta Hihou [Disc 1].asm
+++ Translate/Lupin The Third - Umi Ni Kieta Hihou/Lupin The Third - Umi Ni Kieta Hihou [Disc 1].asm
@@ -163,3 +163,83 @@ TextMain($43A479EC, 10, 0, "Original Work     ")
 TextMain($43A47A0E, 12, 0, "Monkey Punch                  ")
 TextMain($43A47A3E, 12, 0, "(Published by Chuokoron-Shinsha)")
 
+TextMain($43A47AE6, 18, 0, "Lupin III     ")
+TextMain($43A47B0C, 20, 0, "Kanichi Kurita")
+
+TextMain($43A47BAA, 15, 0, "Daisuke Jigen  ")
+TextMain($43A47BCE, 17, 0, "Kiyoshi Kobayashi")
+
+TextMain($43A47C6C, 17, 0, "Ishikawa Goemon")
+TextMain($43A47C92, 19, 0, "Makio Inoue      ")
+
+TextMain($43A47D32, 19, 0, "Fujiko Mine")
+TextMain($43A47D56, 21, 0, "Eiko Masuyama  ")
+
+TextMain($43A47DF6, 15, 0, "Koichi Zenigata")
+TextMain($43A47E1A, 17, 0, "Goro Naya        ")
+
+TextMain($43A47EB8, 19, 0, "Theo   ")
+TextMain($43A47ED8, 21, 0, "Megumi Kubota")
+
+TextMain($43A47F76, 18, 0, "Riana     ")
+TextMain($43A47F98, 20, 0, "Atsuko Enomoto")
+
+TextMain($43A48036, 17, 0, "Professor Clark  ")
+TextMain($43A4805E, 19, 0, "Shunji Fujimura")
+
+TextMain($43A4820E, 8, 0, "Animation Director & Directed by")
+TextMain($43A4823C, 10, 0, "Tomokazu Tokoro               ")
+
+TextMain($43A482E0, 18, 0, "Screenplay")
+TextMain($43A48302, 20, 0, "Taeko Okina   ")
+
+TextMain($43A483A0, 12, 0, "Character Design        ")
+TextMain($43A483CA, 14, 0, "Toshimitsu Kobayashi")
+
+TextMain($43A48468, 18, 0, "Art Director  ")
+TextMain($43A4848E, 20, 0, "Takashi Miyano")
+
+TextMain($43A4852C, 20, 0, "Color Design")
+TextMain($43A48552, 22, 0, "Takae Iijima")
+
+TextMain($43A485F0, 9, 0, "Director of Photography")
+TextMain($43A48616, 11, 0, "Toshihiro Kawada       ")
+
+TextMain($43A486B4, 14, 0, "Recording Director")
+TextMain($43A486DA, 16, 0, "Satoshi Kato      ")
+
+TextMain($43A48778, 15, 0, "Music Supervision")
+TextMain($43A4879E, 17, 0, "Seiji Suzuki     ")
+
+TextMain($43A489CA, 24, 0, "System  ")
+TextMain($43A489F0, 26, 0, "HOWZ            ")
+
+TextMain($43A48A96, 17, 0, "Planning           ")
+TextMain($43A48AC0, 19, 0, "Satoshi Iwasaki")
+
+TextMain($43A48B5E, 20, 0, "Director        ")
+TextMain($43A48B88, 22, 0, "Hajime Okano")
+
+TextMain($43A48C26, 17, 0, "Animation Producer     ")
+TextMain($43A48C54, 19, 0, "Yasumichi Ozaki (TMS)")
+
+TextMain($43A48CF8, 17, 0, "Animation Producer     ")
+TextMain($43A48D26, 19, 0, "Hiromichi Oishi (TMS)")
+
+TextMain($43A48DCA, 23, 0, "Producer       ")
+TextMain($43A48DF6, 25, 0, "Mikio Ono (AAE)")
+
+TextMain($43A48E9A, 16, 0, "Executive Producer          ")
+TextMain($43A48ECC, 18, 0, "Tamotsu Shiina (AAE)  ")
+
+TextMain($43A48F68, 16, 0, "Executive Producer          ")
+TextMain($43A48F9A, 18, 0, "Toshima Masao (AAE)   ")
+
+TextMain($43A49036, 20, 0, "Animation Production")
+TextMain($43A49064, 22, 0, "Tokyo Movie     ")
+
+TextMain($43A490FE, 15, 0, "Planning & Production")
+TextMain($43A49128, 17, 0, "Asmik Ace Entertainment          ")
+
+TextMain($43A491CE, 24, 0, "Production")
+TextMain($43A491F6, 26, 0, "TYCOON      ")
