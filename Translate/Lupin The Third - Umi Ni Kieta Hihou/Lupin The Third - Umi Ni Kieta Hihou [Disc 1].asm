3ef724689e18fe0c051e326cb07a2ec654a44497
PeterLemon <krom@gbadev.org>
2017-02-21 03:34:06 +0000

    Lupin III Translation

--- Translate/Lupin The Third - Umi Ni Kieta Hihou/Lupin The Third - Umi Ni Kieta Hihou [Disc 1].asm
+++ Translate/Lupin The Third - Umi Ni Kieta Hihou/Lupin The Third - Umi Ni Kieta Hihou [Disc 1].asm
@@ -281,3 +281,65 @@ TextMain($43A49E59, 12, 1, "Aburio]   ")
 TextMain($43A49E8F, 2, 0, "What's that, I can't hear you!")
 
 TextMain($43A49F07, 12, 1, "Theo] ")
+
+TextMain($43A49F8F, 12, 1, "Aburio]   ")
+TextMain($43A49FC4, 5, 0, "If something happens,")
+TextMain($43A49FE4, 7, 0, "the mermaid will help us again!")
+
+TextMain($43A4A144, 6, 1, "Kostas]   ")
+TextMain($43A4A174, 12, 0, "... Aburio")
+TextMain($43A4A190, 13, 0, "Hurry up!")
+
+TextMain($43A4A1BA, 12, 1, "Aburio]   ")
+TextMain($43A4A1F0, 10, 0, "Alright!")
+TextMain($43A4A208, 5, 0, "I'm coming back in!")
+
+TextMain($43A4A346, 12, 1, "Kostas]   ")
+TextMain($43A4A388, 10, 0, "Captain, let's get back.")
+TextMain($43A4A3B0, 2, 0, "Report to the professor.")
+
+TextMain($43A4A61E, 16, 1, "Kostas]   ")
+TextMain($43A4A663, 0, 0, "Please take this to the professor,")
+TextMain($43A4A68B, 12, 0, "I will be there later.")
+
+TextMain($43A4A6C1, 12, 1, "Theo] ")
+TextMain($43A4A6F2, 16, 0, "Yep.")
+
+TextMain($43A4A71A, 20, 1, "Aburio]   ")
+TextMain($43A4A757, 25, 0, "Be careful.")
+
+TextMain($43A4A7D1, 8, 1, "Passenger Ferry]")
+TextMain($43A4A808, 0, 0, "Ferry now boarding, we are sailing soon!")
+TextMain($43A4A836, 5, 0, "This is the last ferry today!")
+
+TextMain($43A4A946, 14, 1, "Riana]  ")
+TextMain($43A4A97B, 15, 0, "Gasp.. Gasp..")
+
+TextMain($43A4A9AB, 14, 1, "Riana]  ")
+TextMain($43A4A9E0, 16, 0, "Wait, wait...!")
+
+TextMain($43A4AC04, 20, 1, "Riana]  ")
+
+TextMain($43A4AC73, 6, 1, "Theo] ")
+TextMain($43A4AC9E, 14, 0, "Aargh...")
+
+TextMain($43A4ACC8, 20, 1, "Riana]  ")
+TextMain($43A4AD03, 15, 0, "I want to say it!")
+
+TextMain($43A4AD49, 20, 1, "Riana]  ")
+TextMain($43A4ADAC, 24, 0, "Hey!")
+TextMain($43A4ADCE, 24, 0, "Be careful!!")
+
+TextMain($43A4AE06, 6, 1, "Theo] ")
+TextMain($43A4AE22, 9, 0, "Huh...?")
+
+TextMain($43A4AE6A, 0, 0, "Select")
+TextSave($43A4AE7A, "  That bumped you up!           ")
+TextSave($43A4AE9E, "  Oh, sorry ")
+
+TextMain($43A4AF70, 16, 1, "Riana]  ")
+TextMain($43A4AFB0, 20, 0, "If you hit a girl,")
+TextMain($43A4AFDC, 15, 0, "you should ask her \dAre you OK?\d!")
+
+TextMain($43A4B02E, 6, 1, "Theo] ")
+TextMain($43A4B05A, 9, 0, "Huh...?")
